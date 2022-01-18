b30e2962ce4f32d4f914e523ec705c50c4855fdc
Noah Taggart <noahtag@gmail.com>
2022-01-18 15:23:59 -0600

    working harvest time

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,14 +1,14 @@
 // define variable field initial value is empty array to store plants
-const newField = [ ]
+let fieldArray = [ ]
 
 //define and export function addPlant()
     //addPlant() accept seed object as input add seeds to field array
-export const addPlant = (seedObject, fieldArray)  => {
+export const addPlant = (seedObject)  => {
     fieldArray.push(seedObject)
     return fieldArray
 }
 //define and export usePlants()
     //returns copy of the field array
-export const usePlants = (fieldArray) => {
+export const usePlants = () => {
     return fieldArray
 }
